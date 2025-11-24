144c9b173e25164adaa051aca299b2b8c261641f
luongvietan <luongvietan.231123@gmail.com>
2025-11-25 04:43:07 +0700

    feat: add global CSS with theme variables and custom font utilities, and create an index for section components

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -2,12 +2,4 @@ export { default as HeroSection } from "./hero_section";
 export { default as ProblemSolutionSection } from "./problem_solution_section";
 export { default as ProductSection } from "./product_section";
 export { default as ProblemProductWrapper } from "./problem_product_wrapper";
-export { default as HowItWorksSection } from "./how_it_works_section";
-export { default as FeaturesSection } from "./features_section";
-export { default as FutureSection } from "./future_section";
-export { default as FooterSection } from "./footer_section";
-export { default as FutureFooterSection } from "./future_footer_section";
-export { default as HowItWorksFeaturesWrapper } from "./how_it_works_features_wrapper";
 export { default as UnifiedScrollSection } from "./unified_scroll_section";
-
-
